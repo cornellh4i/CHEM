00f47022633accf4eb56a51ad8b5af6eb7a29c13
Jason Zheng <jasonz4200@gmail.com>
2024-03-25 22:32:56 -0400

    Refine Flowbite components

--- frontend/src/components/templates/CenterTemplate.tsx
+++ frontend/src/components/templates/CenterTemplate.tsx
@@ -9,9 +9,7 @@ const CenterTemplate = ({ children }: CenterTemplateProps) => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="px-6 sm:px-12 py-6 sm:py-12">
-        <div className="max-w-2xl mx-auto">{children}</div>
-      </div>
+      <div className="max-w-2xl mx-auto sm:mt-8 p-4">{children}</div>
     </div>
   );
 };
