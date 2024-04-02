46dc2fc73b6ab9e03d60b08c96f38bd27525636e
Jason Zheng <jasonz4200@gmail.com>
2024-04-02 18:53:27 -0400

    Simplify Button

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -16,10 +16,10 @@ const Button = ({ children, variant = "primary", ...props }: ButtonProps) => {
       "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
     error:
       "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
-  };
+  }[variant];
 
   return (
-    <button className={styles[variant]} {...props}>
+    <button className={styles} {...props}>
       {children}
     </button>
   );
