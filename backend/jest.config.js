7cce65a5270b6eef7e622cdc5eb68e424e17f9de
Jason Zheng <jasonz4200@gmail.com>
2024-04-01 22:26:13 -0400

    Fix Jest and add GitHub workflows

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["src/tests/"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFiles: ["dotenv/config"],
+  moduleFileExtensions: ["js", "ts"],
+  maxWorkers: 1,
+  testTimeout: 30000,
+};
