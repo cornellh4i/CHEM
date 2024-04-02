c7ee919c0c4a670b2463ad0374e242d023ec63a3
Jason Zheng <jasonz4200@gmail.com>
2024-04-02 18:38:28 -0400

    Format frontend with Tailwind changes

--- frontend/src/components/atoms/Select.tsx
+++ frontend/src/components/atoms/Select.tsx
@@ -14,12 +14,19 @@ const Select = (
   if (error) {
     return (
       <div>
-        <label className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">
+        <label
+          className="mb-2 block text-sm font-medium text-red-700
+            dark:text-red-500"
+        >
           {label}
         </label>
         <select
           ref={ref}
-          className="cursor-pointer bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+          className="block w-full cursor-pointer rounded-lg border
+            border-red-500 bg-red-50 p-2.5 text-sm text-red-900
+            placeholder-red-700 focus:border-red-500 focus:ring-red-500
+            dark:border-red-500 dark:bg-gray-700 dark:text-red-500
+            dark:placeholder-red-500"
           {...props}
         >
           {children}
@@ -31,12 +38,19 @@ const Select = (
 
   return (
     <div>
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      <label
+        className="mb-2 block text-sm font-medium text-gray-900
+          dark:text-gray-300"
+      >
         {label}
       </label>
       <select
         ref={ref}
-        className="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full cursor-pointer rounded-lg border border-gray-300
+          bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500
+          focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700
+          dark:text-gray-300 dark:placeholder-gray-400
+          dark:focus:border-blue-500 dark:focus:ring-blue-500"
         {...props}
       >
         {children}
--- frontend/src/components/templates/DefaultTemplate.tsx
+++ frontend/src/components/templates/DefaultTemplate.tsx
@@ -9,7 +9,7 @@ const DefaultTemplate = ({ children }: DefaultTemplateProps) => {
   return (
     <div className="min-h-screen dark:bg-gray-900 dark:text-gray-300">
       <Navbar />
-      <div className="max-w-screen-xl mx-auto sm:mt-8 p-4">{children}</div>
+      <div className="mx-auto max-w-screen-xl p-4 sm:mt-8">{children}</div>
     </div>
   );
 };
