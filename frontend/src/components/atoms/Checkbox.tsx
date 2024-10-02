ed753252fe7bfd1ac53f118b35b439fd79074c86
ashliezhang <janexu770@gmail.com>
2024-10-02 08:13:19 +0000

    change checkbox to radio button, remove focus outline, add black ring

--- frontend/src/components/atoms/Checkbox.tsx
+++ frontend/src/components/atoms/Checkbox.tsx
@@ -1,13 +1,13 @@
 import React, { ReactNode, forwardRef, Ref } from "react";
 
-interface CheckboxProps {
+interface RadioProps {
   label?: ReactNode;
   error?: string;
   [key: string]: any;
 }
 
-const Checkbox = (
-  { defaultChecked, label, error, ...props }: CheckboxProps,
+const RadioButton = (
+  { label, error, ...props }: RadioProps,
   ref: Ref<HTMLInputElement>
 ) => {
   return (
@@ -15,13 +15,12 @@ const Checkbox = (
       <div className="grid h-5 place-items-center">
         <input
           ref={ref}
-          type="checkbox"
-          defaultChecked={defaultChecked}
+          type="radio"
           value=""
-          className="peer col-start-1 row-start-1 h-6 w-6 shrink-0 appearance-none rounded-full border-2 border-black focus:shadow-none focus:outline-none focus:ring-0"
+          className="peer col-start-1 row-start-1 h-3 w-3 cursor-pointer appearance-none rounded-full border-2 border-transparent bg-transparent text-black ring-2 ring-black ring-offset-2 checked:border-transparent checked:bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-black"
           {...props}
         />
-        <div className="pointer-events-none col-start-1 row-start-1 h-3.5 w-3.5 rounded-full focus:shadow-none focus:outline-none focus:ring-0 peer-checked:bg-black" />
+        <div className="pointer-events-none col-start-1 row-start-1 h-3 w-3 rounded-full peer-checked:bg-black" />
       </div>
       <div className="ms-2 text-sm">
         <label className="font-medium text-gray-900 dark:text-gray-300">
@@ -35,4 +34,4 @@ const Checkbox = (
   );
 };
 
-export default forwardRef(Checkbox);
+export default forwardRef(RadioButton);
