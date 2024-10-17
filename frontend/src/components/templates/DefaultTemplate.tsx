462df63d0f3f682f44815940011f5c2cc28588b7
Sanya Mahajan <sm2588@cornell.edu>
2024-10-17 17:13:05 -0400

    Creating new file for DashBoard Template

--- frontend/src/components/templates/DefaultTemplate.tsx
+++ frontend/src/components/templates/DefaultTemplate.tsx
@@ -9,6 +9,7 @@ const DefaultTemplate = ({ children }: DefaultTemplateProps) => {
   return (
     <div className="min-h-screen dark:bg-gray-900 dark:text-gray-300">
       <Navbar />
+
       <div className="mx-auto max-w-screen-xl p-4 sm:mt-8">{children}</div>
     </div>
   );
