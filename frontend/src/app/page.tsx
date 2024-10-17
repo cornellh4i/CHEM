31bd1c9273f22594d2ccb14c6ace58b57388475a
Rohan Sedhain <rs2443@cornell.edu>
2024-10-17 17:29:14 -0400

    Added dummy data for table.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from "next";
+import Table from "@/components/atoms/Table";
+
+const columns: Array<[string, "text" | "number" | "date"]> = [
+  ["Name", "text"],
+  ["Age", "number"],
+  ["Date of Birth", "date"],
+];
+
+const data: Array<string[]> = [
+  ["Alice", "30", "1993-05-12"],
+  ["Bob", "25", "1998-07-08"],
+  ["Charlie", "35", "1988-09-20"],
+];
 
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-center text-6xl font-bold">CHEM</h1>
+      <div>
+        <h1 className="text-center text-6xl font-bold">CHEM</h1>
+        <Table columns={columns} data={data} />
+      </div>
     </div>
   );
 };
