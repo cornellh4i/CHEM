55438952fd58d6d4a5da01734fa5587f8e8ce874
Sanya Mahajan <sm2588@cornell.edu>
2024-10-17 17:36:07 -0400

    Created isCollapsed State

--- frontend/src/components/templates/DashboardTemplate.tsx
+++ frontend/src/components/templates/DashboardTemplate.tsx
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, ReactNode } from "react";
 import DefaultTemplate from "@/components/templates/DefaultTemplate";
 
 import { Home as Dashboard, Segment as Activity, ShowChart as Funds, People as Contributors } from "@mui/icons-material";
 import { Box, IconButton, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+
+interface DashboardTemplateProps {
+    children: ReactNode;
+}
+const DashboardTemplate = ({ children }: DashboardTemplateProps) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const handleSidebarToggle = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+}
+
 
