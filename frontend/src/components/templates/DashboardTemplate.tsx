aef23aafa0e6f41c3f91cc6f138788952eedee15
Sanya Mahajan <sm2588@cornell.edu>
2024-10-17 17:30:04 -0400

    Adding necessary imports and importing icons from MUI

---
+++ frontend/src/components/templates/DashboardTemplate.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import DefaultTemplate from "@/components/templates/DefaultTemplate";
+
+import { Home as Dashboard, Segment as Activity, ShowChart as Funds, People as Contributors } from "@mui/icons-material";
+import { Box, IconButton, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+
