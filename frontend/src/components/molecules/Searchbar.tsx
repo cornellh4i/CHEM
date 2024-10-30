b9aa5e664d5f0651bae18f69cacad155ef8d5638
Sanya Mahajan <sm2588@cornell.edu>
2024-10-30 17:27:45 -0400

    Modified searchbar to make it correct size and add icon

--- frontend/src/components/molecules/Searchbar.tsx
+++ frontend/src/components/molecules/Searchbar.tsx
@@ -1,45 +1,49 @@
 import React, { useState } from "react";
-import Input from "../../components/atoms/Input";
+import Input from "../../components/atoms/Input"; // Assuming this is a styled TextField component or similar
 import SearchIcon from "@mui/icons-material/Search";
-import Button from "../../components/atoms/Button";
-import { InputAdornment, IconButton } from "@mui/material";
+import { InputAdornment, IconButton, TextField } from "@mui/material";
 
 interface SearchBarProps {
-  onSearch: (query: string) => void;
-  placeholder?: string;
-  width?: string;
+    onSearch: (query: string) => void;
+    placeholder?: string;
+    width?: string;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({
-  onSearch,
-  placeholder = "Search for...",
-  width = "100%",
+    onSearch,
+    placeholder = "Search for...",
+    width = "300px",
 }) => {
-  const [query, setQuery] = useState("");
+    const [query, setQuery] = useState("");
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-  };
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+    };
 
-  const handleSearch = () => {
-    onSearch(query);
-  };
+    const handleSearch = () => {
+        onSearch(query);
+    };
 
-  return (
-    <Input
-      value={query}
-      onChange={handleInputChange}
-      placeholder={placeholder}
-      width={width}
-      endAdornment={
-        <InputAdornment position="end">
-          <IconButton onClick={handleSearch} edge="end">
-            <SearchIcon />
-          </IconButton>
-        </InputAdornment>
-      }
-    />
-  );
+    return (
+        <TextField
+            value={query}
+            onChange={handleInputChange}
+            placeholder={placeholder}
+            variant="outlined"
+            style={{
+                width: width,
+            }}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleSearch} edge="end">
+                            <SearchIcon />
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    );
 };
 
 export default SearchBar;
