d4c6fca95f8d4aff12b2ee494a5ea4a2e244859e
Nicole Qiu <nfq2@cornell.edu>
2025-02-28 04:16:09 +0000

    Add empty settings page

--- frontend/src/components/molecules/AddContributorModal.tsx
+++ frontend/src/components/molecules/AddContributorModal.tsx
@@ -22,7 +22,7 @@ const AddContributorModal: React.FC<AddContributorModalProps> = ({
 
   const handleAdd = () => {
     // Add contributor logic here
-    handleClose(); 
+    handleClose();
   };
 
   return (
@@ -30,7 +30,7 @@ const AddContributorModal: React.FC<AddContributorModalProps> = ({
       <DialogTrigger asChild onClick={handleOpen}>
         {children}
       </DialogTrigger>
-      <DialogContent className="rounded-lg border border-gray-300 bg-white p-8 shadow-lg sm:min-h-[475px] sm:max-w-lg">
+      <DialogContent className="border-gray-300 bg-white rounded-lg border p-8 shadow-lg sm:min-h-[475px] sm:max-w-lg">
         <DialogHeader>
           <DialogTitle className="mb-4 text-lg font-semibold">
             Add a Contributor
@@ -40,66 +40,66 @@ const AddContributorModal: React.FC<AddContributorModalProps> = ({
           <div>
             <label
               htmlFor="name"
-              className="mb-1 block text-sm font-medium text-black"
+              className="text-black mb-1 block text-sm font-medium"
             >
               Name
             </label>
             <input
               id="name"
               placeholder="Naomi Rufian"
-              className="w-full rounded-md border border-gray-400 p-2 text-sm text-black focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className="border-gray-400 text-black focus:ring-gray-500 w-full rounded-md border p-2 text-sm focus:outline-none focus:ring-2"
             />
           </div>
           <div>
             <label
               htmlFor="phone"
-              className="mb-1 block text-sm font-medium text-black"
+              className="text-black mb-1 block text-sm font-medium"
             >
               Phone number
             </label>
             <input
               id="phone"
               placeholder="+1 (718) 123-4567"
-              className="w-full rounded-md border border-gray-400 p-2 text-sm text-black focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className="border-gray-400 text-black focus:ring-gray-500 w-full rounded-md border p-2 text-sm focus:outline-none focus:ring-2"
             />
           </div>
           <div>
             <label
               htmlFor="email"
-              className="mb-1 block text-sm font-medium text-black"
+              className="text-black mb-1 block text-sm font-medium"
             >
               Email
             </label>
             <input
               id="email"
               placeholder="123abc@gmail.com"
-              className="w-full rounded-md border border-gray-400 p-2 text-sm text-black focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className="border-gray-400 text-black focus:ring-gray-500 w-full rounded-md border p-2 text-sm focus:outline-none focus:ring-2"
             />
           </div>
           <div>
             <label
               htmlFor="address"
-              className="mb-1 block text-sm font-medium text-black"
+              className="text-black mb-1 block text-sm font-medium"
             >
               Mailing address
             </label>
             <input
               id="address"
               placeholder="123 ABC Drive, Queens NY 11357"
-              className="w-full rounded-md border border-gray-400 p-2 text-sm text-black focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className="border-gray-400 text-black focus:ring-gray-500 w-full rounded-md border p-2 text-sm focus:outline-none focus:ring-2"
             />
           </div>
           <div className="flex justify-between space-x-4 pt-8">
             <Button
-              variant="outline"
-              className="rounded-md border border-gray-400 px-6 py-2 text-sm text-black hover:bg-gray-100"
+              // variant="outline"
+              className="border-gray-400 text-black hover:bg-gray-100 rounded-md border px-6 py-2 text-sm"
               onClick={handleClose} // Close modal on Cancel
             >
               Cancel
             </Button>
             <Button
               variant="primary"
-              className="rounded-md border border-gray-700 bg-gray-700 px-8 py-2 text-sm text-white hover:bg-gray-800"
+              className="border-gray-700 bg-gray-700 text-white hover:bg-gray-800 rounded-md border px-8 py-2 text-sm"
               onClick={handleAdd} // Close modal on Add
             >
               Add
