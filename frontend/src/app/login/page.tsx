2952f705ed955a2e7c74efc1cfc8e782d76e2591
Sydney Chin <158599418+nysdey@users.noreply.github.com>
2025-03-15 20:06:23 +0000

    Created /login/page.tsx in app folder

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginFormCard from "@/components/molecules/LoginFormCard";
+import LoginForm from "@/components/molecules/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <LoginFormCard />
+    </div>
+  );
+};
+
+export default LoginPage;
