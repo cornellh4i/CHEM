83e1e455577861dcf15ecc534368c66da206b9bf
Ashlie Zhang <az468@cornell.edu>
2025-04-11 20:12:50 +0000

    add toggle button

--- frontend/src/app/fundsMain/page.tsx
+++ frontend/src/app/fundsMain/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import * as React from "react";
 import DashboardTemplate from "@/components/templates/DashboardTemplate";
 import ContributorsTable from "@/components/molecules/ContributorsTable";
 import AddIcon from "@mui/icons-material/Add";
 import LaunchIcon from "@mui/icons-material/Launch";
 import SettingsIcon from "@mui/icons-material/Settings";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Button from "@/components/atoms/Button";
 import AddContributorModal from "@/components/molecules/AddContributorModal";
 import SearchBar from "@/components/molecules/Searchbar";
@@ -19,6 +24,15 @@ const FundsMainPage = () => {
     setSearchQuery(query);
   };
 
+  const [alignment, setAlignment] = React.useState<string | null>('left');
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null,
+  ) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <DashboardTemplate>
       <div className="flex flex-col justify-items-center">
@@ -29,7 +43,7 @@ const FundsMainPage = () => {
               <SettingsIcon fontSize="small" style={{ marginRight: 6 }} />
               Settings
             </Button>
-            <Button>
+            <Button variant="secondary">
               Create new fund
               <KeyboardArrowDownIcon />
             </Button>
@@ -41,24 +55,22 @@ const FundsMainPage = () => {
             <SearchBar onSearch={handleSearch} width="50%" />
           </div>
 
-          <AddContributorModal>
-            <Button
-              variant="primary"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                height: "40px",
-                padding: "0 16px",
-                marginBottom: 8,
-              }}
-            >
-              Add Contributor
-              <AddIcon style={{ marginLeft: "6px" }} />
-            </Button>
-          </AddContributorModal>
+          <Button
+            variant="secondary"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "40px",
+              padding: "0 16px",
+              marginBottom: 8,
+            }}
+          >
+            Add Contributor
+            <AddIcon style={{ marginLeft: "6px" }} />
+          </Button>
 
           <Button
-            variant="primary"
+            variant="secondary"
             icon={<LaunchIcon />}
             style={{
               display: "flex",
@@ -70,6 +82,20 @@ const FundsMainPage = () => {
           >
             Export
           </Button>
+
+          <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+          >
+            <ToggleButton value="left" aria-label="left aligned">
+              <ViewModuleIcon />
+            </ToggleButton>
+            <ToggleButton value="center" aria-label="centered">
+              <ViewListIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
         </div>
 
         <ContributorsTable searchQuery={searchQuery} />
