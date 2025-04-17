02977fd1ae19be1855a92251cdbb21f5039a5a00
Leon Huang <147883914+leonhuang29@users.noreply.github.com>
2025-04-16 23:42:01 -0400

    Leon/Rohan Funds Table (#154)
    * Did the ticket
    
    * oops
    
    ---------
    
    Co-authored-by: Nicole Qiu <152471190+nfq2@users.noreply.github.com>

--- frontend/src/app/funds/page.tsx
+++ frontend/src/app/funds/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+
 import DashboardTemplate from "@/components/templates/DashboardTemplate";
+import FundsTable from "@/components/molecules/FundsTable";
 
 const FundsPage = () => {
   return (
     <DashboardTemplate>
-      <div>Funds Page</div>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Funds Page</h1>
+        <FundsTable />
+      </div>
     </DashboardTemplate>
   );
 };
