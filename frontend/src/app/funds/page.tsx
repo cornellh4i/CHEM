6e2c8077279c95bf627b0fc81dccc234c03164c6
Alisha Varma <69208001+alishav14@users.noreply.github.com>
2025-04-21 21:38:13 -0400

    Add Funds Modal (#147)
    * created addfundmodal and basic structure for funds page
    
    * fix
    
    ---------
    
    Co-authored-by: tmavani23 <138815500+tmavani23@users.noreply.github.com>
    Co-authored-by: Tanvi Mavani <tm669@cornell.edu>

--- frontend/src/app/funds/page.tsx
+++ frontend/src/app/funds/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
 import DashboardTemplate from "@/components/templates/DashboardTemplate";
+import AddFundModal from "@/components/molecules/AddFundModal";
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@/components/atoms/Button";
 import FundsTable from "@/components/molecules/FundsTable";
 
 const FundsPage = () => {
   return (
     <DashboardTemplate>
-      <div className="p-6">
-        <h1 className="text-2xl font-bold mb-4">Funds Page</h1>
+      <div className="flex flex-col px-8 py-6">
+        {/* Top section with heading and Add Fund button */}
+        <div className="mb-12 flex items-center justify-between">
+          <h1 className="text-3xl">Funds</h1>
+          <AddFundModal>
+            <Button
+              variant="primary"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: "40px",
+                padding: "0 16px",
+              }}
+            >
+              Add Fund
+              <AddIcon style={{ marginLeft: "6px" }} />
+            </Button>
+          </AddFundModal>
+        </div>
+
+        {/* Placeholder for future fund table */}
+        <div className="text-gray-500 pl-1 text-left text-lg">
+          No funds to display yet.
+        </div>
         <FundsTable />
       </div>
     </DashboardTemplate>
