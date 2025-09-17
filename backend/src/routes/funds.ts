6857bd81f073ec9e172db792e1f304f617d19e98
Akhil Kagithapu <90364898+akh1lk@users.noreply.github.com>
2025-09-17 15:03:08 -0400

    Akhil jade/funds endpoints (#183)
    * sign post
    
    * getTransactionsByFundId controller + route
    
    * - added deleteFundById (controller & route)
    
    * corrected deleteFundById to delete all realted transactions
    
    * removed todos
    
    ---------
    
    Co-authored-by: Jade Lee <yl3873@cornell.edu>

--- backend/src/routes/funds.ts
+++ backend/src/routes/funds.ts
@@ -46,12 +46,71 @@ fundRouter.put("/:id", async (req, res) => {
   // implement route here
 });
 
-// TODO: delete new fund
 fundRouter.delete("/:id", async (req, res) => {
-  // implement route here
+  try {
+    const fund = await controller.deleteFundById(req.params.id);
+    if (!fund) {
+      return res.status(404).json({ error: "Fund not found" });
+    }
+    res.status(200).json({ message: "Fund deleted" });
+  } catch (error) {
+    console.error(error);
+    const errorResponse: ErrorMessage = {
+      error: error instanceof Error ? error.message : "Failed to delete fund",
+    };
+    res.status(500).json(errorResponse);
+  }
 });
 
-// TODO: get all transactions by fund id
+fundRouter.get("/:id/transactions", async (req, res) => {
+  try {
+    // Get inputs
+    const sortBy = req.query.sortBy as string;
+    const order = req.query.order as string;
+    const { id } = req.params;
+    // Validate inputs
+    const validSortField = new Set(["date", "amount"]);
+    const validOrders = new Set(["asc", "desc"]);
+
+    // Extract sort, and pagination from query parameters
+    const sort =
+      sortBy && validSortField.has(sortBy)
+        ? {
+            field: sortBy as "date" | "amount",
+            order: validOrders.has(order) ? (order as "asc" | "desc") : "asc",
+          }
+        : undefined;
+    const pagination = {
+      skip: req.query.skip ? Number(req.query.skip) : undefined,
+      take: req.query.take ? Number(req.query.take) : undefined,
+    };
+
+    // Fetch contributors from database using sort and pagination
+    const { transactions, total } = await controller.getTransactionsByFundId(
+      id,
+      sort,
+      pagination
+    );
+
+    // Return contributors with total count
+    res.status(200).json({ transactions, total });
+  } catch (error) {
+    console.error(error);
+    const errorResponse: ErrorMessage = {
+      error:
+        error instanceof Error
+          ? error.message
+          : "Failed to get funds's transactions",
+    };
+    // 404 org not found
+    if (errorResponse.error == "Fund not found") {
+      res.status(404).json(errorResponse);
+    } else {
+      // all other errors
+      res.status(500).json(errorResponse);
+    }
+  }
+});
 
 /// TODO: get all contributors by fund id
 
