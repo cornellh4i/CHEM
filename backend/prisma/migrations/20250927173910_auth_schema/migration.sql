0798ad49ba5c950bf0f7933a9a74a7755dc3d88b
Nicole Qiu <nfq2@cornell.edu>
2025-09-27 17:40:06 +0000

    auth schema migrations

---
+++ backend/prisma/migrations/20250927173910_auth_schema/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[firebaseUid]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `firebaseUid` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `organizationId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firebaseUid" TEXT NOT NULL,
+ADD COLUMN     "organizationId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebaseUid_key" ON "User"("firebaseUid");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
