b846378c1d6f13386cb08275607204db565fa45f
Akhil Kagithapu <finalakhil@gmail.com>
2025-11-13 21:42:56 +0000

    added proper login signup redirect flow functionality

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signUp, login, logout } from "../controllers/auth";
+import { signUp, login, logout, checkEmail } from "../controllers/auth";
 import auth from "../middleware/auth";
 
 const router = Router();
@@ -7,6 +7,8 @@ const router = Router();
 // middleware
 router.post("/signup", auth, signUp);
 router.get("/login", auth, login);
+// public existence check by email
+router.get("/check-email", checkEmail);
 router.post("/logout", auth, logout);
 
 export default router;
