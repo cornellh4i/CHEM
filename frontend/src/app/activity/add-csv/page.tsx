78d6dda466d8c6d43bdfd433c8d6ef2144ade5f2
Nathnael Tesfaw <nbt26@cornell.edu>
2026-04-12 14:57:39 -0400

    quick fix

--- frontend/src/app/activity/add-csv/page.tsx
+++ frontend/src/app/activity/add-csv/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import DashboardTemplate from "@/components/templates/DashboardTemplate";
